df48d083c4798b51c59cf069d89e3b2a2276a728
johnnywwy <johnnywwy@gmail.com>
2022-02-14 17:03:17 +0800

    updata

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import x from "./x.js";
 console.log("hi~", x);
-console.log("7777");
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "XDML - 写代码啦",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
-

3c2e646451a0d80776262cb524364a6e0ff3f88d
johnnywwy <johnnywwy@gmail.com>
2022-02-15 01:04:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import x from "./x.js";
 import png from "./assets/1.png";
 console.log(png);
 
-console.log("x: ", x);
+// console.log("x: ", x);
 const div = document.querySelector("#app");
 // div.innerHTML = `
 // <img src='${png}'></img>
